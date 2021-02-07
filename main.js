68e433739367b882e2e58859b25a19fc18e82280
JohnPower <stkautoru@mail.ru>
2021-02-07 15:36:01 +0700

    message broadcasting done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { createRequire } = require('module');
 
-const socket = require('socket.io');
+const sock = require('socket.io');
 
 const server = require('http').createServer( ((req, res) => {
     res.writeHead(200, {
@@ -9,22 +9,69 @@ const server = require('http').createServer( ((req, res) => {
     res.end('');
 }));
 
-const io = socket(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
-    path: '',
-    serveClient: false,
-    // below are engine.IO options
-    pingInterval: 10000,
-    pingTimeout: 5000,
-    cookie: false
-});
+class ChatClient {
+    id = '';
+    userName = '';
+
+    constructor(id, userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+}
+
+const generateId = () => Math.random() * 1000;
+
+class ChatApp {
+
+    connected = [];
+
+    constructor(server) {
+        const io = sock(server, {
+            cors: {
+                origin: "http://localhost:3000",
+                methods: ["GET", "POST"],
+            },
+            path: '',
+            serveClient: false,
+            pingInterval: 10000,
+            pingTimeout: 5000,
+            cookie: false
+        });
+
+        io.on("connection", (socket) => {
+            console.log("Made socket connection");
+
+            socket.username = "Anonym";
+
+            let id = generateId();
+            this.connected.push(new ChatClient(id, socket.username));
+
+            socket.on('change_username', (data) => {
+                console.log("change_username");
+                const index = this.connected.findIndex((item) => item.id === id);
+                this.connected[index].userName = data.username;
+                socket.username = data.username;
+            });
+
+            socket.on('send_message', (arg) => {
+                console.log("new message");
+                console.log(arg);
+                socket.broadcast.emit('new_message', {message:arg.message, client: arg.client,name: socket.username});
+            });
 
 
-io.on("connection", function (socket) {
-    console.log("Made socket connection");
-});
 
-server.listen(4000);
+            socket.on('disconnect',() => {
+                this.connected = this.connected.filter((item) => item.id !== id);
+            });
+        });
+
+
+
+        server.listen(4000);
+    }
+}
+
+
+const app = new ChatApp(server);
+
